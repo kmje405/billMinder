4c555ad6a47706d7c1074ce57afee0e78583bc91
K.M. Jones-Eastland <kmje405@gmail.com>
2024-02-27 11:30:10 -0700

    Refactor bills controller to improve readability and add new features

--- src/bills/bills.controller.ts
+++ src/bills/bills.controller.ts
@@ -1,37 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Param,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { BillsService } from './bills.service';
 import { CreateBillDto } from './dto/create-bill.dto';
-import { UpdateBillDto } from './dto/update-bill.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { GetUserById } from 'src/decorators/get-user-by-id.decorator';
+import { UpdateBillDto } from './dto/update-bill.dto';
 
 @Controller('bills')
+@UseGuards(AuthGuard('jwt'))
 export class BillsController {
-  constructor(private readonly billsService: BillsService) {}
+  constructor(private readonly billsService: BillsService) { }
 
-  @UseGuards(AuthGuard('jwt'))
-  @Post()
-  create(@Body() createBillDto: CreateBillDto, @GetUserById('id') userID: string){
-    return this.billsService.create(createBillDto, userID);
-  }
-
+  // Get bills for the authenticated user
   @Get()
-  findAll() {
-    return this.billsService.findAll();
+  getUserBills(@GetUserById() userId: string) {
+    return this.billsService.getUserBills(userId);
   }
 
+  // Get a specific bill by ID
+  // bills/123
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.billsService.findOne(+id);
+    console.log('id', id);
+    return this.billsService.findOne(id);
   }
-
+  // Create a new bill
+  @Post()
+  create(@Body() createBillDto: CreateBillDto, @GetUserById() userId: string) {
+    return this.billsService.create(createBillDto, userId);
+  }
+  // Update a specific bill
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateBillDto: UpdateBillDto) {
-    return this.billsService.update(+id, updateBillDto);
+    return this.billsService.update(id, updateBillDto);
   }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.billsService.remove(+id);
+  // Mark a bill as paid
+  @Patch('paid/:id')
+  markAsPaid(@Param('id') id: string) {
+    return this.billsService.markBillAsPaid(id);
   }
 }
